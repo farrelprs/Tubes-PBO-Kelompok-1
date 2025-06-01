499cf54691af68deb9909553bfca011d35ac4357
BintangKantata <52192720+BintangKantata@users.noreply.github.com>
2025-06-01 13:33:16 +0700

    Update EveryTask.java

--- ToDoList/ToDoList/src/main/java/com/pboToDoList/ToDoList/task/EveryTask.java
+++ ToDoList/ToDoList/src/main/java/com/pboToDoList/ToDoList/task/EveryTask.java
@@ -1,12 +1,15 @@
 package com.pboToDoList.ToDoList.task;
 
+import com.pboToDoList.ToDoList.user.RegularUser;
+
 import java.util.List;
+import java.util.Optional;
 
 public interface EveryTask {
     public abstract void addTask(Task task);
-    public abstract void updateTask(String taskTitle, Task updatedTask);
-    public abstract void deleteTask(String taskTitle);
-    public abstract Task getTask(int taskId);
+    public abstract void updateTask(Task updatedTask);
+    public abstract void deleteTask(int taskId);
     public abstract List<Task> getAllTasks();
-
+    List<Task> getTasksForUser(RegularUser user);
+    Optional<Task> findById(int taskId);
 }
