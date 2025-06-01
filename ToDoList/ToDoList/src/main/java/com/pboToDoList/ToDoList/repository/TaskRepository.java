1529dc6d3162b06712238f0e3f90dab28ee5dd7a
BintangKantata <52192720+BintangKantata@users.noreply.github.com>
2025-06-01 13:35:23 +0700

    Update TaskRepository.java

--- ToDoList/ToDoList/src/main/java/com/pboToDoList/ToDoList/repository/TaskRepository.java
+++ ToDoList/ToDoList/src/main/java/com/pboToDoList/ToDoList/repository/TaskRepository.java
@@ -7,13 +7,9 @@ import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
 import java.util.List;
-import java.util.Optional;
 
 @Repository
 public interface TaskRepository extends JpaRepository<Task, Integer> {
-    Optional<Task> findByTitle(String title);
-    void deleteByTitle(String title);
-    List<Task> findByPriority(TaskPriority priority);
-    List<Task> findByRuser(RegularUser ruser); // fixed
+    List<Task> findByRuser(RegularUser ruser);
 }
 
